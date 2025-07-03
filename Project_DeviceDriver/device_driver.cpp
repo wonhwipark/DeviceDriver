ab94d0b3f181547fd9df0b346a6e3eb22f383fc4
wonhwipark <wonhwipark02@gmail.com>
2025-07-03 16:49:34 +0900

    driver

--- Project_DeviceDriver/device_driver.cpp
+++ Project_DeviceDriver/device_driver.cpp
@@ -1,17 +1,33 @@
 #include "device_driver.h"
+#include <exception>
+
+class ReadFiveTimeFail : public std::exception {};
+class WriteWhenRead0xFF : public std::exception {};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {
 }
 
-int DeviceDriver::read(long address)
-{
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+int DeviceDriver::read(long address) {
+    int result = (int)(m_hardware->read(address));
+    postConditionCheck(result, address);
+    return result;
+}
+
+void DeviceDriver::postConditionCheck(int ret, long address) {
+    for (int turn = 0; turn < TestTryCount; turn++) {
+        int testValue = (int)(m_hardware->read(address));
+        if (ret != testValue) {
+            throw ReadFiveTimeFail();
+        }
+    }
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
+    int testValue = (int)(m_hardware->read(address));
+    if (testValue != 0xFF) {
+        throw WriteWhenRead0xFF();
+    }
     m_hardware->write(address, (unsigned char)data);
 }
